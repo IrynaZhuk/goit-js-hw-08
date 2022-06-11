496e3056bf0b87e726e41a04fab859a278f501cc
IrynaZhuk <irynazh94@gmail.com>
2022-06-11 18:55:50 +0300

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+      localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000)
+  );
+  
+  const data = localStorage.getItem('videoplayer-current-time') ?? 0;
+  
+  player.setCurrentTime(data);
