5a6e11a2b5e32d16aa54a8e8129adb93085c05c6
IrynaZhuk <irynazh94@gmail.com>
2022-06-11 20:13:51 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+const throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+
+const emailItem = form.elements.email;
+const messageItem = form.elements.message;
+
+enterDataIntoForm();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  const email = emailItem.value;
+  const message = messageItem.value;
+
+  localStorage.setItem('formData', JSON.stringify({ email, message }));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem('formData')));
+  localStorage.removeItem('formData');
+}
+
+function enterDataIntoForm() {
+  const savedMessage = localStorage.getItem('formData');
+  if (savedMessage) {
+    const formData = JSON.parse(savedMessage);
+    emailItem.value = formData.email;
+    messageItem.value = formData.message;
+  }
+}
+
