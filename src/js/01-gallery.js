51d9574343aa186d77d090404c084e4526878d76
IrynaZhuk <irynazh94@gmail.com>
2022-06-11 18:34:57 +0300

    lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,53 +9,17 @@ const items = createGallaryItems(galleryItems);
 
 gallaryContainer.insertAdjacentHTML('beforeend', items);
 
-gallaryContainer.addEventListener('click', onGallaryContainerClickHandler)
-
 function createGallaryItems(galleryItems) {
   return galleryItems.map(({preview, original, description}) => {
-        return `
-      <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+    return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
   }).join("")  
 }
 
-let instance = "";
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-function onGallaryContainerClickHandler(event) {  
-  event.preventDefault();
-  
-  if (event.target.nodeName !== "IMG") {
-    return
-  }
-  
-  else {
-   instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-    instance.show()
-  };
-
-  window.addEventListener('keydown', onEscKeyPress);
-}
-
-
-
-function onCloseModal() {
-  window.removeEventListener('keydown', onEscKeyPress);
-instance.close()
-}
-
-function onEscKeyPress(event) {
-  if (event.code === 'Escape') {
-    onCloseModal()
- }
-}
 console.log(galleryItems);
